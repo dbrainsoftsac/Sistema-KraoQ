5d58f5b16dc56a521ddf6ea5bc092daf7dd566d7
Luis Guillermo Zanelli <gzanelliv@gmail.com>
2016-09-29 01:31:18 +0000

    modificaciones

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+  layout 'internal', except: [:index]
+  def index
+  end
+
+  def canciones
+  end
+
+  def piqueos
+  end
+
+  def contact
+  end
+end
